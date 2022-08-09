88af50536adc143df9e6828fb25885c6ef232715
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2022-08-09 10:17:43 +0430

    portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,26 +6,27 @@ const About = () => {
       name="about"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-8 max-auto flex flex-col justify-center w-full h-full md:pl-20">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
-        <p className="text-xl mt-20">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-          quaerat optio aut fugit sunt consequuntur voluptate modi impedit qui
-          molestias quas et corrupti debitis sit, illum accusantium aliquam
-          nihil illo? Inventore atque sit officia nemo quae
+
+        <p className="text-xl mt-20 text-center">
+          Throughout my career as a front-end developer, I've emphasized the
+          importance of scalable and well-documented code. I enjoy working
+          collaboratively but can also run with projects on my own. Excited
+          about the prospect of joining a product-driven company like Acme Corp.
         </p>
+
         <br />
-        <p className="text-xl">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
-          illum culpa ipsum rerum excepturi! Omnis ipsum id veritatis molestiae
-          accusantium consequatur dolorem esse quia doloremque, nobis reiciendis
-          repellat illum sapiente recusandae assumenda iure tenetur quis error
-          molestias culpa fugit eos illo? Sint quia veritatis placeat accusamus,
-          accusantium voluptatem consequuntur molestiae?
+
+        <p className="text-xl text-center">
+          Experienced front-end developer who is comfortable working with
+          JavaScript, HTML/CSS to deliver exceptional customer experiences.
+          Looking for an opportunity to work at a company like Acme Corp with a
+          positive social mission to democratize online payments
         </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,41 +1,47 @@
-import { useState } from "react";
-import heroImage from "./../assets/heroImage.jpeg";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import React from "react";
+import HeroImage from "./../assets/heroImage.jpeg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 "
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white sm:mt-20">
-            I'm a FrontEnd Developer😎
+          <h2 className="text-4xl sm:text-7xl font-bold text-white header">
+            I'm a Junior front-end Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have one years of experience building and desighing software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS
+            Junior front-end developer with 1+ years of experience designing and
+            building responsive web design in the financial industry. Proficient
+            with CSS and JS Frameworks, with extensive knowledge of UX and user
+            psychology. Notable achievements include boosting the conversion
+            rate of an existing website by 18% with improved code and design.
           </p>
-          <button
-            to="portfolio"
-            smooth
-            duration={500}
-            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-          >
-            Portfolio
-            <span className="group-hover:rotate-90 duration-300">
-              <MdKeyboardArrowRight size={25} className="ml-1" />
-            </span>
-          </button>
+
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
         </div>
 
         <div>
           <img
-            src={heroImage}
-            alt="my Profile"
-            className="rounded-2xl mx-auto w-full h-64 md:w-96 md:h-96 md:my-40"
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
           />
         </div>
       </div>
